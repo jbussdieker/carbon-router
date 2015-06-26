a1a9eb38a5c3d251db727c57ac912e141d12d498
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-06-26 13:57:53 -0700

    Initial commit

---
+++ lib/carbon/router.rb
@@ -0,0 +1,7 @@
+require "carbon/router/version"
+
+module Carbon
+  module Router
+    # Your code goes here...
+  end
+end
---
+++ lib/carbon/router/version.rb
@@ -0,0 +1,5 @@
+module Carbon
+  module Router
+    VERSION = "0.0.1"
+  end
+end
